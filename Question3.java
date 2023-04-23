a96292a9cca634cd0c85a4852ee696fe35fa8c07
ShahidRahmat <shahid.rahmats@gmail.com>
2023-04-23 12:49:57 +0800

    Changes

--- Question3.java
+++ Question3.java
@@ -9,8 +9,9 @@ public class Question3
      * 
      * Hint: in.nextInt() to read integer
      */
-     
+    System.out.print("Enter an integer: ");
     Scanner in = new Scanner(System.in);
-    
+    int num = in.nextInt();
+    System.out.println(num*num);
   }
 }
--- Question5.java
+++ Question5.java
@@ -25,8 +25,34 @@ public class Question5
      *     2
      * Hint: Use a loop to get input. Use another 2 loops to find the mode
      */
-     
+    System.out.print("Enter a number: ");
     Scanner in = new Scanner(System.in);
-    
-  }
+    int numinput = in.nextInt();
+
+    int[] numbers = new int[numinput];
+    for (int l = 0; l<numinput; l++){
+      System.out.print("Enter another number: ");
+      Scanner in2 = new Scanner(System.in);
+      int currentnum = in2.nextInt();
+      numbers[l] = currentnum;
+    }
+
+    int mode = 0;
+    int modecount = 0;
+    for (int i = 0; i< numinput; i++){
+      int count = 0;
+
+      for (int j = 0; j<numinput; j++){
+        if (numbers[j] == numbers[i]) count++;
+      }
+
+      if (count>modecount){
+        modecount = count;
+        mode = numbers[i];
+      }
+
+      }
+      System.out.print(mode);
+
+    }
 }
